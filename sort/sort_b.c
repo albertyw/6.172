d8a0ad6a5cb7e6dfccb7315c3685e929d0b04e83
Albert Y Wang <albertyw@tobramycin.csail.mit.edu>
2011-09-25 15:15:46 -0400

    Changed spacing, added writeup text for 2.5

--- sort/sort_b.c
+++ sort/sort_b.c
@@ -10,14 +10,14 @@ static inline void copy_b(data_t * source, data_t * dest, int n) ;
 /* Basic merge sort */
 void sort_b(data_t *A, int p, int r) 
 {
-	assert (A) ;
-	if (p < r)
-	{
-		int q = (p + r) / 2 ;
-		sort_b(A, p, q);
-		sort_b(A, q + 1, r);
-		merge_b(A, p, q, r) ;
-	}	
+  assert (A) ;
+  if (p < r)
+  {
+    int q = (p + r) / 2 ;
+    sort_b(A, p, q);
+    sort_b(A, q + 1, r);
+    merge_b(A, p, q, r) ;
+  }  
 }
 
 /* A merge routine. Merges the sub-arrays A [p..q] and A [q + 1..r].
@@ -25,71 +25,69 @@ void sort_b(data_t *A, int p, int r)
  */
 static inline void merge_b(data_t *A, int p, int q, int r) 
 { 
-	assert(A) ;
-	assert(p <= q) ;
-	assert((q + 1) <= r) ;
-	int n1 = q - p + 1;
-	int n2 = r - q ;
+  assert(A) ;
+  assert(p <= q) ;
+  assert((q + 1) <= r) ;
+  int n1 = q - p + 1;
+  int n2 = r - q ;
 
-	data_t * left = 0, * right = 0 ; 
-	mem_alloc(&left, n1 + 1) ;
-	mem_alloc(&right, n2 + 1) ;
-        if (left == NULL || right == NULL)
-        {
-                mem_free (&left) ;
-                return ;
-        }
-	
-	copy_b (&(A [p]), left, n1) ;
-	copy_b (&(A [q + 1]), right, n2) ; 
-	left [n1] = UINT_MAX ;
-	right [n2] = UINT_MAX ;
+  data_t * left = 0, * right = 0 ; 
+  mem_alloc(&left, n1 + 1) ;
+  mem_alloc(&right, n2 + 1) ;
+  if (left == NULL || right == NULL)
+  {
+    mem_free (&left) ;
+    return ;
+  }
+  
+  copy_b (&(A [p]), left, n1) ;
+  copy_b (&(A [q + 1]), right, n2) ; 
+  left [n1] = UINT_MAX ;
+  right [n2] = UINT_MAX ;
 
-	// int i = 0 ;
-	// int j = 0 ;
-	int k = p;
+  // int i = 0 ;
+  // int j = 0 ;
+  int k = p;
 
-	unsigned int * __restrict leftptr = left;
-	unsigned int * __restrict rightptr = right;
+  unsigned int * __restrict leftptr = left;
+  unsigned int * __restrict rightptr = right;
 
-	for ( ; k <= r ; k++)
-	  {
-	    /*
-	    *(A+k) = *leftptr;
-	    leftptr++;
+  for ( ; k <= r ; k++)
+  {
+  *(A+k) = *leftptr;
+  leftptr++;
+  if (*rightptr < *(A+k)){
+    *(A+k) = *rightptr;
+    rightptr++;
+    leftptr--;
+  }
+      
+    /*
+       {
+    if (*leftptr >= *rightptr)
+    {
+      *(A+k) = *rightptr;
+      rightptr++;
+    }
+    else
+    {
+      *(A+k) = *leftptr;
+      leftptr++;
+    }
+    */
+  }
 
-	    if (*rightptr < *(A+k)){
-		*(A+k) = *rightptr;
-		rightptr++;
-		leftptr--;
-	    }
-	    */
-	  /*
-     	{
-		if (*leftptr >= *rightptr)
-		{
-		  *(A+k) = *rightptr;
-		  rightptr++;
-		}
-		else
-		{
-		  *(A+k) = *leftptr;
-		  leftptr++;
-		}
-	  */
-	}
-
-	mem_free(&left) ;
-	mem_free(&right);
+  mem_free(&left) ;
+  mem_free(&right);
 }
 
 static inline void copy_b(data_t * source, data_t * dest, int n)                              
 {                                                                             
-        assert (dest) ;                                                       
-        assert (source) ;                                                     
-        int i ;                                                               
-        for (i = 0 ; i < n ; i++)                                             
-        {                                                                     
-                dest [i] = source [i] ;                                       
-        }                                                                     
+  assert (dest) ;                                                       
+  assert (source) ;                                                     
+  int i ;                                                               
+  for (i = 0 ; i < n ; i++)                                             
+  {                                                                     
+    dest [i] = source [i] ;                                       
+  }                                                                     
 }
