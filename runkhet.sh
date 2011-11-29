127884b766aa3c62e2bd8d52da0fd455888677f1
Albert Y Wang <albertyw@zanamivir.csail.mit.edu>
2011-11-29 00:21:24 -0500

    Added some instructions to runkhet

--- runkhet.sh
+++ runkhet.sh
@@ -4,6 +4,9 @@
 # so that you don't have to input the setup commands 
 # each time you want to run it
 
+# Feel free to change the commands within the 
+# khetcommands file to suit your testing needs
+
 # Run using "./runkhet.sh" or "sh runkhet.sh"
 # (you may need to first run "chmod 774 runkhet.sh" first)
 cat khetcommands | ./khetplayer
